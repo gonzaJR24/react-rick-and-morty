733c9f679b7e00283cf18546fc020d02f79accc6
Junior <andresjunior240498hehe@gmail.com>
2023-01-19 22:33:42 -0400

    chuck-jokes

--- src/App.js
+++ src/App.js
@@ -2,33 +2,25 @@ import './App.css';
 import {useEffect,useState} from "react"
 
 function App() {
-  const [data,setData]=useState([])
+  const [joke,setJoke]=useState("")
+  const url="https://api.chucknorris.io/jokes/random"
+
+const next=()=>{
+  const request=fetch(url)
+  request
+    .then(res=>res.json())
+    .then(res=>setJoke(res.value))
+    .catch(()=>console.log("error"))
+}
 
   useEffect(()=>{
-    const url="https://randomuser.me/api/?results=3"
-    const request=fetch(url)
-      request
-      .then(data=>data.json())
-      .then(data=>data.results.map(person=>
-        setData(e=>
-        [...e,<div key={person.cell} className="card">
-          <div>{person.name.first} {person.name.last}</div>
-          <div><img src={person.picture.large} alt="" /></div>
-        </div>])
-        ))
-      .catch(()=>console.log("error"))
+   next()
   },[])
+
   return (
     <>
-    <div className="container">
-      <div className="title">
-        <h1>Empleados del mes</h1>
-        <p>⭐</p>
-      </div>
-      <div className="data">
-      {data}
-      </div>
-    </div>
+    <h1>{joke}</h1>
+    <button onClick={next}>next joke</button>
     </>
   );
 }
